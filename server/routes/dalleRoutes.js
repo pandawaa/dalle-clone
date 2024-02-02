1ba6087ca6b19ddec3e3b7ed227fd3597ccff890
pandawaa <pallakangoding@gmail.com>
2024-02-02 14:11:57 +0800

    modified route Delle

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import * as dotenv from "dotenv"
-import { OpenAIApi, Configuration } from "openai"
+import { Configuration, OpenAIApi } from "openai"
 
 dotenv.config()
 
@@ -13,7 +13,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration)
 
 router.route("/").get((req, res) => {
-  res.status(200).json({ message: "Hello From Dalle Ai" })
+  res.status(200).json({ message: "Hello from DALL-E!" })
 })
 
 router.route("/").post(async (req, res) => {
@@ -30,7 +30,7 @@ router.route("/").post(async (req, res) => {
     const image = aiResponse.data.data[0].b64_json
     res.status(200).json({ photo: image })
   } catch (error) {
-    console.log(error)
+    console.error(error)
     res
       .status(500)
       .send(error?.response.data.error.message || "Something went wrong")
