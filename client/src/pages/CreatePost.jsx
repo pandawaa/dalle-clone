95b24ae280fe3a414b9504e2c5ea7e612bda2298
pandawaa <pallakangoding@gmail.com>
2024-02-03 13:26:37 +0800

    modified Create Post

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -21,15 +21,18 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true)
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            ...form,
-          }),
-        })
+        const response = await fetch(
+          "https://dalle-clone-swart.vercel.app/api/v1/post",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              ...form,
+            }),
+          }
+        )
 
         await response.json()
         alert("Success")
